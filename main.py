898f7492ac3117940a1b6ec72ba9474cf14f7b3d
Luis Manuel Aviles Altamar <lumaaviles0@gmail.com>
2022-09-22 17:25:44 -0500

    agregados templates y archivos estaticos Se ha agregado las funcionalidades de deteccion cacheo de errors 404 y 500

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-from flask import Flask
+from flask import Flask, request, redirect, make_response, render_template
 
 app = Flask(__name__)
 
+todos = [
+    'Realizar los commit de los avances',
+    'Completar el Curso de FastAPI',
+    'Iniciar el curso de Git/Github'
+    ]
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def not_found(error):
+    return render_template('500.html', error=error)
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+
+    return render_template('hello.html', **context)
